351c141ee18b32d8804977fe2177bd2aee32a97d
Styn Vercauteren <styn.vercauteren@outlook.com>
2024-02-06 09:43:39 +0100

    add not found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,7 +1,16 @@
-// TODO: Implement a not found page.
+import { Link } from "react-router-dom";
 
-const NotFound = () => {
-  return false;
-};
-
+/* eslint-disable react/no-unescaped-entities */
+const NotFound = () => (
+  <main>
+    <h1 className="text-3xl">
+      Looks like you've stirred the wrong drink. This page must be on the rocks.
+      Let's shake things up and get you{" "}
+      <Link to="/" className="font-bold">
+        back to the homepage
+      </Link>
+      , where the real party is! 🍸
+    </h1>
+  </main>
+);
 export default NotFound;
